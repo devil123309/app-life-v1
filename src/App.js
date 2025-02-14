f453b35f35ad95be60e4c3ecedf710a3217dfa0c
devil123309 <manojsudini868@gmail.com>
2025-02-15 00:25:22 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import "./pages/Style.css";
+ 
 import Header from "./pages/Header";
+import Content from "./pages/Content";  
+
 function App() {
   return (
     <>
       <Header/>
+      <Content/>
+       
       </>
   );
 }
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,11 +1,54 @@
-import React, { PureComponent } from 'react'
+import React from "react";
 
-export default class index extends PureComponent {
-  render() {
-    return (
-      <div>
-        hello hi
+const Index = () => {
+  const hamburg = () => {
+    console.log("Hamburger menu clicked");
+  };
+
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="logo" data-aos="zoom-in" data-aos-duration="1500">
+          Life <span>Connect</span>
+        </div>
+        <div className="links">
+          <div
+            className="link"
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-delay="100"
+          >
+            <a href="#">Home</a>
+          </div>
+          <div
+            className="link"
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-delay="200"
+          >
+            <a href="#">About</a>
+          </div>
+          <div
+            className="link"
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-delay="300"
+          >
+            <a href="#">Donors</a>
+          </div>
+          <div
+            className="link"
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-delay="500"
+          >
+            <a href="#">Patient</a>
+          </div>
+        </div>
+        <i className="fa-solid fa-bars hamburg" onClick={hamburg}></i>
       </div>
-    )
-  }
-}
+    </nav>
+  );
+};
+
+export default Index;

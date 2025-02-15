1df8e20ac03146ad2eb2c1a95f2487bac30bd6cd
devil123309 <manojsudini868@gmail.com>
2025-02-15 19:48:44 +0530

    navigation2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./pages/Header";
 import Content from "./pages/Content";
-import About from "./pages/About"; // ✅ Make sure this path is correct
-import "./pages/Style.css";
+import About from "./pages/About";  
+import Donor from "./pages/Donor";  
+import "./pages/Style.css"; // Ensure the correct path
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Content />} />
-        <Route path="/about" element={<About />} /> {/* ✅ Fixed */}
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/about" element={<About />} /> 
+          <Route path="/donors" element={<Donor />} /> 
+        </Routes>
+      </main>
     </Router>
   );
 };

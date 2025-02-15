7974415e26bacc3015337f98cc18b6f2ece5daa2
Ram Sahith <devil123309@gmail.com>
2025-02-16 00:37:06 +0530

    hiiiiiiiiiii

--- src/pages/hi.js
+++ src/pages/hi.js
@@ -1,1 +1,14 @@
-ffdddddddddddd
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class hi extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>hi</div>
+    )
+  }
+}
+
+export default hi
